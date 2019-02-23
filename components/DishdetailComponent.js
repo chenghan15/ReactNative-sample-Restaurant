1982202c0be77d847bc252c5fd925fa0229d80dd
hanw <hanwfifteen@gmail.com>
2019-02-23 14:24:35 +1300

    W3 Ass3T3

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -32,6 +32,13 @@ function RenderDish(props){
             return false;
     }
 
+    const recognizeComment = ({ moveX, moveY, dx, dy }) => {
+        if ( dx > 200 )
+            return true;
+        else
+            return false;   
+    }
+
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: (e, gestureState) => {
             return true;
@@ -53,6 +60,10 @@ function RenderDish(props){
                     ],
                     { cancelable: false }
                 );                
+            }
+            else if(recognizeComment(gestureState))
+            {
+                props.onPressComment();
             }
 
             return true;
